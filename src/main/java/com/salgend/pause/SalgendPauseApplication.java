074d5cf38ed1464704c43de7f940a0d02015e0c5
s.zerradi <salmane.zerradi@de2s.com>
2024-06-20 21:00:06 +0100

    Install project

---
+++ src/main/java/com/salgend/pause/SalgendPauseApplication.java
@@ -0,0 +1,13 @@
+package com.salgend.pause;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalgendPauseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalgendPauseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salgend/pause/SalgendPauseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salgend.pause;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalgendPauseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
